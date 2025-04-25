451da5949fa223555d755396ce603c81f296c4c5
sofiasaless <sofiasaleswk@gmail.com>
2025-04-25 19:19:07 -0300

    page: selecionar o tipo de cozinha e cardTipoCozinha

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Inicio from './pages/Inicio/Inicio';
+import TiposCozinha from './pages/TiposCozinha/TiposCozinha';
 
 function App() {
 
   return (
-    <Inicio />
+    // <Inicio />
+    <TiposCozinha />
   )
 }
 
